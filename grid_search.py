3feb281d83edcbef12dda2dae9391acddf0e9dbc
aemilani <aealieftekhary@gmail.com>
2020-07-28 18:12:03 +0200

    intermediate

--- grid_search.py
+++ grid_search.py
@@ -25,12 +25,15 @@ batch = [512, 1024]
 opt = ['adam', 'nadam']
 
 result = []
-
+ctr = 1
 for h1 in hidden_1:
     for h2 in hidden_2:
         for h3 in hidden_3:
             for h4 in hidden_4:
                 for d in dense:
+                    print('Architecture setting {} out of 32 ...'.format(ctr))
+                    ctr += 1
+                    
                     np.random.seed(13)
                     time = str(datetime.datetime.now()).split()[1]
                     
@@ -57,21 +60,82 @@ for h1 in hidden_1:
                     cb = [ea, cp]
                     
                     model.fit(x_train, y_train, batch_size=1024, epochs=30,
-                                        validation_data=(x_valid, y_valid), callbacks=cb)
+                                        validation_data=(x_valid, y_valid),
+                                        callbacks=cb, verbose=0)
                     
                     model = load_model('grid_chp/{}.h5'.format(time))
                     
                     score = model.evaluate(x_test, y_test, verbose=0)
                     test_loss = score[0]
                     test_acc = score[1]
-                    dic = {'hidden_1': h1, 'hidden_2': h2, 'hidden_3': h3,
-                           'hidden_4': h4, 'dense': d}
+                    dic = {'h1': h1, 'h2': h2, 'h3': h3, 'h4': h4, 'd': d}
                     result.append((dic, test_loss, test_acc))
 
 result.sort(key=lambda x: x[1])
-print('Best setting (loss):', result[0][0])
-print('Best score (loss):', result[0][1], result[0][2])
+best_loss = result[0]
 
 result.sort(key=lambda x: x[2])
-print('Best setting (acc):', result[-1][0])
-print('Best score (acc):', result[-1][1], result[-1][2])
+best_acc = result[-1]
+
+if best_loss == best_acc:
+    best = [best_loss]
+else:
+    best = [best_loss, best_acc]    
+
+result_final = []
+ctr = 1
+for res in best:
+    arc = res[0]
+    h1, h2, h3, h4, d = arc['h1'], arc['h2'], arc['h3'], arc['h4'], arc['d']
+    for b in batch:
+        for o in opt:
+            print('Hyperparameter setting {} out of 4'.format(ctr))
+            ctr += 1
+            
+            np.random.seed(13)
+            time = str(datetime.datetime.now()).split()[1]
+            
+            K.clear_session()
+            model = Sequential()
+            model.add(Conv2D(h1, (3, 3), padding='same', activation='relu', input_shape=(wm_dim, wm_dim, 3)))
+            model.add(MaxPooling2D())
+            model.add(Conv2D(h2, (3, 3), padding='same', activation='relu'))
+            model.add(MaxPooling2D())
+            model.add(Conv2D(h3, (3, 3), padding='same', activation='relu'))
+            model.add(MaxPooling2D())
+            model.add(Conv2D(h4, (3, 3), padding='same', activation='relu'))
+            model.add(MaxPooling2D())
+            model.add(Flatten())
+            model.add(Dropout(0.25))
+            model.add(Dense(d, activation='relu'))
+            model.add(Dropout(0.5))
+            model.add(Dense(8, activation='softmax'))
+            
+            model.compile(o, loss='categorical_crossentropy', metrics=['categorical_accuracy'])
+            
+            ea = EarlyStopping(patience=5)
+            cp = ModelCheckpoint('grid_chp/{}.h5'.format(time), save_best_only=True)
+            cb = [ea, cp]
+            
+            model.fit(x_train, y_train, batch_size=b, epochs=30,
+                                validation_data=(x_valid, y_valid), callbacks=cb)
+            
+            model = load_model('grid_chp/{}.h5'.format(time))
+            
+            score = model.evaluate(x_test, y_test, verbose=0)
+            test_loss = score[0]
+            test_acc = score[1]
+            dic = {'h1': h1, 'h2': h2, 'h3': h3, 'h4': h4, 'd': d, 'b': b, 'o': o}
+            result_final.append((dic, test_loss, test_acc))
+            
+result_final.sort(key=lambda x: x[1])
+best_loss = result[0]
+
+result_final.sort(key=lambda x: x[2])
+best_acc = result[-1]
+
+print('Best setting (loss):', best_loss[0])
+print('Best score (loss):', best_loss[1], best_loss[2])
+
+print('Best setting (acc):', best_acc[0])
+print('Best score (acc):', best_acc[1], best_acc[2])
